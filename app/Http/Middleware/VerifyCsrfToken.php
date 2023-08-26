92edcb7fc366cccb3bc07a9e347c27bfd18d70f4
HuNan-Jasonccs <1521910992@qq.com>
2023-08-26 10:51:37 +0800

    增加中英文翻译

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -14,4 +15,10 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+//    public function handle($request, Closure $next)
+//    {
+//        return $this->addCookieToResponse($request, $next($request));
+//    }
+
 }
