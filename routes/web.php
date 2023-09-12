9dcd4c98645470ce523e02c12338cd8a4d3bad83
HuNan-Jasonccs <1521910992@qq.com>
2023-09-12 16:26:37 +0800

    根据 ip 获取定位的城市

--- routes/web.php
+++ routes/web.php
@@ -39,41 +39,7 @@ Route::post('/b', function (Request $request) {
 });
 
 //抛出预期的错误，例如请求字段格式错误
-
 Route::get('/c', [Controller::class, 'store'])->middleware('validation');
-
-//，d法 抛出意外的错误。
-Route::post('/d', function (Request $request) {
-    $c = 0;
-    $res = 1/$c;
-});
-
-//  使用URL查询参数's'进行以下逻辑测试。
-Route::get('/e', function (Request $request) {
-    $s = $request->input('s');
-
-    $stack = [];
-    $brackets = [
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-    ];
-
-    foreach (str_split($s) as $char) {
-        if (in_array($char, array_values($brackets))) {
-            $stack[] = $char;
-        } elseif (in_array($char, array_keys($brackets))) {
-            if (empty($stack) || array_pop($stack) !== $brackets[$char]) {
-                return JsonResponse::error('Invalid brackets', 400);
-            }
-        }
-    }
-    if (empty($stack)) {
-        return JsonResponse::success('Valid brackets');
-    } else {
-        return JsonResponse::error('Invalid brackets', 400);
-    }
-});
 
 // 根据IP 获取定位的城市
 Route::get('/f', function (Request $request) {
