1086b2624ab396dca8385bffbbdd5c1d44ac8a6b
jsonccs-home <1521910992@qq.com>
2023-09-20 00:05:35 +0800

    登录/login2 之后 设置cookie 来获取访问/telescope 监控面板

--- app/Http/Middleware/TelescopeAuthMiddleware.php
+++ app/Http/Middleware/TelescopeAuthMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Models\utils\JsonResponse;
 use Closure;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Exceptions\JWTException;
 use \Larke\Admin\Middleware\Authenticate as LarkeAdminAuth ;
@@ -20,11 +21,11 @@ class TelescopeAuthMiddleware extends LarkeAdminAuth{
         try {
             $origin = $request->server('REQUEST_URI') ?: '';
             if (Str::startsWith($origin,'/telescope')){
-//                dd(111);
-                $request->headers->set('Authorization', 'Bearer ');
+                $cookies = $request->cookie('token');
+                $request->headers->set('Authorization', 'Bearer '.$cookies);
             }
         } catch (\Exception $e) {
-             return JsonResponse::error('222222222',Response::HTTP_OK);
+             return JsonResponse::error($e->getMessage(),Response::HTTP_OK);
         }
         return $next($request);
     }
